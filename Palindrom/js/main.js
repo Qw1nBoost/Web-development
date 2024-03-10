93c6a1012329a889c268655b7ca2bc536dd40903
Andrey <andreikgd2000@gmail.com>
2024-03-10 17:51:07 +0200

    fixe bug

--- Palindrom/js/main.js
+++ Palindrom/js/main.js
@@ -2,12 +2,19 @@ let checkButton = document.getElementById("check-btn");
 let input = document.getElementById("text-input");
 
 
-checkButton.addEventListener("click", () => {
+checkButton.addEventListener("click", function () {
     f = true;
     start = 0;
     end = input.value.length - 1;
     while (end > start) {
-        input.value[start].toUpperCase() != input.value[end].toUpperCase() ? f = true : start++, end--;
+        if (input.value[start].toUpperCase() != input.value[end].toUpperCase()) {
+            f = false;
+            break
+        } else {
+            start++;
+            end--;
+        }
+        //input.value[start].toUpperCase() != input.value[end].toUpperCase() ? f = true : start++, end--;
     }
     f == true ? alert("Palindrom") : alert("Not Palindrom");
 });
